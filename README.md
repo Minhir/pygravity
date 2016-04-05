b427213dda8d96f5a4d923c885bb1ee9c8127d7f
Vladimir Ivakin <vl.ivakin@yandex.ru>
2016-04-05 20:08:58 +0600

    Add comments to example

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Simple gravity simulation module.  
 
 pygravity.py -- the module itself.
+
 solarsystem.py -- usage example, model of Solar System.
--- solarsystem.py
+++ solarsystem.py
@@ -1,6 +1,6 @@
 from pygravity import Planet, World
 
-
+# Creation of planets.
 sun = Planet(0, 0, 1.9885 * 10**30, 0, 0, 'sun')
 mercury = Planet(46 * 10 ** 9, 0, 3.33 * 10 ** 23, 0, 47360, 'mercury')
 venus = Planet(107.5 * 10 ** 9, 0, 4.9775 * 10 ** 24, 0, 35000, 'venus')
@@ -13,7 +13,7 @@ neptune = Planet(4452.94 * 10 ** 9, 0, 1.0243 * 10**26, 0, 5434.9, 'neptune')
 asteroid = Planet(4452.94 * 10 ** 8, 4452.94 * 10 ** 8, 1.0243 * 10**8, -438, -5434.9, 'asteroid')
 
 
-world = World(60*5)     # Set step of time in seconds
+world = World(60*5)     # Set step of time in seconds.
 world.add_planets(sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, asteroid)
-world.output_data_config('data.dat', 1000)
-world.run(1000000)
+world.output_data_config('data.dat', 1000)  # Print output data in 'data.dat'. Every 1000 step.
+world.run(1000000)      # Run simulation with 1000000 steps.
