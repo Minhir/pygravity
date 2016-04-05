e27d639fe6710eceb830e97a9043ee7e3084dea1
Vladimir Ivakin <vl.ivakin@yandex.ru>
2016-04-05 17:55:32 +0600

    Add simple doc

--- pygravity.py
+++ pygravity.py
@@ -1,6 +1,15 @@
 class Planet:
 
     def __init__(self, x, y, m, v_x, v_y, name):
+        """
+        Args:
+            x (float): x coordinate of planet.
+            y (float): y coordinate of planet.
+            m (float): Mass of planet.
+            v_x (float): Velocity x projection.
+            v_y (float): Velocity y projection.
+            name (string): Name of the planet.
+        """
         self.x = x
         self.y = y
         self.m = m
@@ -12,25 +21,34 @@ class Planet:
 class World:
 
     def __init__(self, step=0.001, G=6.67*10**-11):
+        """
+        Args:
+            step (int): Time step of simulation.
+            G (float): Gravitational constant.
+        """
         self.step = step
         self.G = G
         self.planet_list = []
         self.print_data = False
 
     def distance(self, p1, p2):
+        """Count distance between two planets."""
         return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
 
     def output_data_config(self, file_name='data.dat', split=100):
+        """Configure output data file."""
         self.print_data = True
         self.data_file = open(file_name, 'w')
         self.split = split
 
     def force_between(self, p1, p2):
+        """Count force between two planets."""
         distance = self.distance(p1, p2)
         F = p2.m / distance ** 2
         return F * (p2.x - p1.x) / distance, F * (p2.y - p1.y) / distance
 
     def add_planets(self, *args):
+        """Add planets to world."""
         for i in args:
             self.planet_list.append(i)
 
@@ -57,11 +75,15 @@ class World:
             self.planet_list[i].v_y += speed_delta_y
 
     def run(self, number_of_steps):
+        """Run simulation.
+        Args:
+            number_of_steps (int): Number of simulation steps.
+        """
         for i in range(number_of_steps):
             self.step_count()
             if self.print_data:
                 if i % self.split == 0:
-                    print(1. * i / number_of_steps)
+                    print(1. * i / number_of_steps, '%')
                     print_list = []
                     for j in self.planet_list:
                         print_list.append(j.x)
